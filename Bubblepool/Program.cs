6260901475a2eff3f573d1cb1421c9fb0bdd5b3d
mathkarl <mathkarl@LSE-B2725>
2020-02-06 11:15:43 +0000

    Bubblepool

--- Bubblepool/Program.cs
+++ Bubblepool/Program.cs
@@ -15,21 +15,12 @@ namespace Simhall
         }
         public static void BubblePoolTest(String[] args)
         {
-            BubblePool newPool = new BubblePool(5);
+            SaveBubblePool newPool = new SaveBubblePool(5);
             Swimmer johan = new Swimmer();
             for (int i = 4; (i > 0); i--)
             {
                 newPool.GetIn(new Swimmer());
-            }
-            try
-            {
-                newPool.GetIn(johan);
                 // här kastas PoolDirtyException.
-            }
-            catch
-            {
-                Console.WriteLine("All swimmers has to leave the pool!");
-                newPool.Clean();
             }
             newPool.GetIn(johan);
             newPool.GetIn(new Swimmer());
--- Bubblepool/SaveBubblePool.cs
+++ Bubblepool/SaveBubblePool.cs
@@ -10,19 +10,35 @@ namespace Simhall
     {
         private string notification;
 
+        private int dirt;
+
+        public int people;
+
         public SaveBubblePool(int max) : base(max)
         {
-
+            notification = "SafeBubblePool cleaned itself automatically!";
         }
 
         public override void GetIn(Swimmer joiner)
         {
+            this.GetDirtier();
             base.GetIn(joiner);
         }
 
         public override void getOut(Swimmer leaver)
         {
             base.getOut(leaver);
+        }
+
+        private void GetDirtier()
+        {
+            this.dirt++;
+            if ((this.dirt == 3))
+            {
+                this.Clean();
+                this.dirt = 0;
+                Console.WriteLine(notification);
+            }
         }
     }
 }
